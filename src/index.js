347d4511461a21cc18c9b7d30fd319f9d1f53362
Данил Выступов <davaimne58@gmail.com>
2026-05-24 23:24:14 +0300

    Added express server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import express from 'express';
 import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const TOKEN = process.env.BOT_TOKEN;
+const PORT = process.env.PORT || 3000;
+
+const bot = new Telegraf(TOKEN);
 
 bot.start((ctx) => {
   ctx.reply('Привет! Я твой бот 🤖');
@@ -13,7 +17,22 @@ bot.command('hello', (ctx) => {
   ctx.reply('Hello!🚀');
 });
 
-bot.launch();
+bot.launch({
+  webhook: {
+    domain: '',
+    port: PORT,
+  },
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
